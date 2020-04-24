2c4917973a04c7bba0563824411528dc2ee01c17
best <M__Java@163.com>
2020-04-24 20:08:25 +0800

    no message

--- src/main/java/com/example/studyspring/cn/WebController.java
+++ src/main/java/com/example/studyspring/cn/WebController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
